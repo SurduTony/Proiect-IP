7cc03bf4450c2d8bb1d678750b97ec1465b7dcb3
SacrieriuRazvanMarcian <101807901+SacrieriuRazvanMarcian@users.noreply.github.com>
2023-04-29 19:45:22 +0300

    Delete dll-uri directory

--- dll-uri/Oras/Oras/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Oras
-{
-    //Oras.dll
-    public class Oras
-    {
-        protected int _id; // numarul de ordine al orasului 
-        protected int _name; //numele orasului
-    }
-}
