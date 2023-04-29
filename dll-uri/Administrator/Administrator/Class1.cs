659068dea9998ab2d2292deb4a3b59990817470b
SacrieriuRazvanMarcian <razvan-marcian.sacrieriu@student.tuiasi.ro>
2023-04-29 19:09:23 +0300

    dll-uri

---
+++ dll-uri/Administrator/Administrator/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Administrator
+{
+    public class GestiuneAdministrator
+    {
+        protected int _idAdmin; //nr. de ordine al admin. (acesta poate introduce restaurante,sali etc.)
+        protected string _name; //numele (user)
+        protected string _password;//parola de logare
+    }
+}
