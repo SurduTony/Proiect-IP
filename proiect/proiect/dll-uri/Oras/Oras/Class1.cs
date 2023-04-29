81471f7fb0c9f40005a524a04616e2049aef30fa
SacrieriuRazvanMarcian <razvan-marcian.sacrieriu@student.tuiasi.ro>
2023-04-29 19:11:06 +0300

    dll-uri v2

---
+++ proiect/proiect/dll-uri/Oras/Oras/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Oras
+{
+    //Oras.dll
+    public class Oras
+    {
+        protected int _id; // numarul de ordine al orasului 
+        protected int _name; //numele orasului
+    }
+}
---
+++ proiect/proiect/dll-uri/Restaurant/Restaurant/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    //Clasa restaurant 
+    public class Restaurant
+    {
+        protected int _id; //numarul de ordine (primary key)
+        protected string _name; //numele restaurantului
+        protected string _address; //unde se afla restaurantul
+        protected string _city; //orasul unde este restaurantul
+        protected string _menu; //meniul (mancare)
+
+    }
+}
